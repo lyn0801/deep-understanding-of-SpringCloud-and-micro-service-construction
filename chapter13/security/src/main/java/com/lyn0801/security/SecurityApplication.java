16d7924eabd6a9ccac81ba9a0a5cfcace4befaee
liuyanan <liu.yanan@marstor.com>
2018-10-16 17:03:32 +0800

    2018-10-16 17:00 springboot security package war

--- chapter13/security/src/main/java/com/lyn0801/security/SecurityApplication.java
+++ chapter13/security/src/main/java/com/lyn0801/security/SecurityApplication.java
@@ -2,11 +2,20 @@ package com.lyn0801.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SecurityApplication {
+public class SecurityApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecurityApplication.class, args);
 	}
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
+    }
+        
+        
 }
